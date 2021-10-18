c674e1a0f7b606e100b61e9d7924e7bb4e449c6f
katyaugust28 <katyaugust28@gmail.com>
2021-10-17 20:55:22 -0700

    debugging 10.17.3

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,4 +1,4 @@
-import React { useState } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
